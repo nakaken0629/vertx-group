c5933345f7b40b0ce0865ee231abc97633631aa9
NAKAGAKI Kenji <nakaken0629@gmail.com>
2017-03-23 23:46:06 +0900

    modify using vertx.web

--- vertx-sample/src/main/java/red/itvirtuoso/vertx/sample/MyFirstVerticle.java
+++ vertx-sample/src/main/java/red/itvirtuoso/vertx/sample/MyFirstVerticle.java
@@ -2,16 +2,26 @@ package red.itvirtuoso.vertx.sample;
 
 import io.vertx.core.AbstractVerticle;
 import io.vertx.core.Future;
+import io.vertx.core.http.HttpServerResponse;
+import io.vertx.ext.web.Router;
 
 public class MyFirstVerticle extends AbstractVerticle {
     @Override
     public void start(Future<Void> fut) {
+        // Create a router object.
+        Router router = Router.router(vertx);
+
+        // Bind "/" to our hello message - so we are still compatible.
+        router.route("/").handler(routingContext -> {
+            HttpServerResponse response = routingContext.response();
+            response
+                    .putHeader("content-type", "text/html")
+                    .end("<h1>Hello from my first Vert.x 3 application (*2)</h1>");
+        });
+
         vertx
                 .createHttpServer()
-                .requestHandler(r -> {
-                    r.response().end("<h1>Hello from my first " +
-                            "Vert.x 3 application</h1>");
-                })
+                .requestHandler(router::accept)
                 .listen(config().getInteger("http.port", 8080),
                         result -> {
                             if (result.succeeded()) {
