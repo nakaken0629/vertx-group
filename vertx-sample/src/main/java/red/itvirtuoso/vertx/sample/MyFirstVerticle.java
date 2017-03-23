a172d72a541549a9decf0a09e824fd6263bfcd21
NAKAGAKI Kenji <nakaken0629@gmail.com>
2017-03-24 08:04:46 +0900

    add rest api

--- vertx-sample/src/main/java/red/itvirtuoso/vertx/sample/MyFirstVerticle.java
+++ vertx-sample/src/main/java/red/itvirtuoso/vertx/sample/MyFirstVerticle.java
@@ -3,36 +3,60 @@ package red.itvirtuoso.vertx.sample;
 import io.vertx.core.AbstractVerticle;
 import io.vertx.core.Future;
 import io.vertx.core.http.HttpServerResponse;
+import io.vertx.core.json.Json;
 import io.vertx.ext.web.Router;
+import io.vertx.ext.web.RoutingContext;
 import io.vertx.ext.web.handler.StaticHandler;
+import red.itvirtuoso.vertx.sample.first.Whisky;
+
+import java.util.LinkedHashMap;
+import java.util.Map;
 
 public class MyFirstVerticle extends AbstractVerticle {
+    // Store our product
+    private Map<Integer, Whisky> products = new LinkedHashMap<>();
+
+    // Create some product
+    private void createSomeData() {
+        Whisky bowmore = new Whisky("Bowmore 15 Years Laimrig", "Scotland, Islay");
+        products.put(bowmore.getId(), bowmore);
+        Whisky talisker = new Whisky("Talisker 57° North", "Scotland, Island");
+        products.put(talisker.getId(), talisker);
+    }
+
     @Override
     public void start(Future<Void> fut) {
+        createSomeData();
+
         // Create a router object.
         Router router = Router.router(vertx);
-
-        // Bind "/" to our hello message - so we are still compatible.
-        router.route("/").handler(routingContext -> {
-            HttpServerResponse response = routingContext.response();
-            response
-                    .putHeader("content-type", "text/html")
-                    .end("<h1>Hello from my first Vert.x 3 application (*2)</h1>");
-        });
 
         // Serve static resources from the /assets directory
         router.route("/assets/*").handler(StaticHandler.create("assets"));
 
+        // Rest of the method
+        router.get("/api/whiskies").handler(this::getAll);
+
         vertx
                 .createHttpServer()
                 .requestHandler(router::accept)
-                .listen(config().getInteger("http.port", 8080),
+                .listen(
+                        // Retrieve the port from the configuration,
+                        // default to 8080.
+                        config().getInteger("http.port", 8080),
                         result -> {
                             if (result.succeeded()) {
                                 fut.complete();
                             } else {
                                 fut.fail(result.cause());
                             }
-                        });
+                        }
+                );
+    }
+
+    private void getAll(RoutingContext routingContext) {
+        routingContext.response()
+                .putHeader("content-type", "application/json; charset=utf-8")
+                .end(Json.encodePrettily(products.values()));
     }
 }
